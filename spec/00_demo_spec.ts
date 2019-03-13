a3df7a584107b62c5ae1e13566a9739a3feddb8e
Archana <48483890+abobba@users.noreply.github.com>
2019-03-13 14:01:04 +0000

    added files

---
+++ spec/00_demo_spec.ts
@@ -0,0 +1,12 @@
+describe('Writing a specit', () => {
+    it('is easy when you know how', () => {
+        //Given - Arrange
+        const a = 10, b = 20;
+
+        //When - Act
+        const answer = a + b;
+
+        //Then - Assert
+        expect(answer).toBe(31);
+    });
+});
